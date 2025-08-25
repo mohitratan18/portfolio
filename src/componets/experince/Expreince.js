bf7ed2042aa4ef55c3304de929333b82725080f5
mohit <mohitratan2003@gmail.com>
2025-08-25 22:05:25 +0530

    added m2p experience

--- src/componets/experince/Expreince.js
+++ src/componets/experince/Expreince.js
@@ -1,12 +1,13 @@
 import React from "react";
 import bm from "./buildingminds_technologies_logo.jpg";
+import m2p from '../../componets/assests/m2plogo.png'
 const Expreince = () => {
   return (
 
     <div className="flex flex-col  items-center">
         <p className="text-[#0f969c] text-lg">EXPERIENCE</p>
         <div
-          className="flex justify-center items-center gap-4"
+          className="flex justify-center items-center  gap-4 md:gap-[120px] w-[40vw] "
           style={{
             padding: "30px",
             borderRadius: "15px",
@@ -27,8 +28,35 @@ const Expreince = () => {
               color: "#0f969c",
             }}
           >
-            <p className="text-lg">Building Minds Technology - Sep 2024 - Present</p>
+            <p className="text-lg">Building Minds Technology - Sep 2024 - March 2025</p>
             <p>Full Stack WebDeveloper</p>
+          </div>
+        </div>
+
+        <div
+          className="flex justify-center items-center  gap-4 md:gap-[120px] w-[40vw] "
+          style={{
+            padding: "30px",
+            borderRadius: "15px",
+            backgroundColor: "#030F12",
+            margin: "3rem",
+            border: "1px solid #0f969c",
+            boxShadow: "-0px -0px 15px #0f969c",
+            cursor: "pointer",
+            transition: "all 0.2s",
+          }}
+        >
+          <div>
+            <img src={m2p} alt="logo" />
+          </div>
+          <div
+            className="flex flex-col gap-4 items-start"
+            style={{
+              color: "#0f969c",
+            }}
+          >
+            <p className="text-lg">M2P - March 2025 - Present</p>
+            <p>SDE - 1</p>
           </div>
         </div>
     </div>
